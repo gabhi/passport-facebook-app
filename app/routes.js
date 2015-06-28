6c46f37821278ed486756943d2ec9377efc1099b
Abhijit Gaikwad <AGaikwad@walmartlabs.com>
2015-06-27 22:00:49 -0700

    test

--- app/routes.js
+++ app/routes.js
@@ -3,8 +3,12 @@ module.exports = function(app, passport) {
     // normal routes ===============================================================
 
     // show the home page (will also have our login links)
-    app.get('/', function(req, res) {
-        res.render('index.ejs');
+    // app.get('/', function(req, res) {
+    //     res.render('index.ejs');
+    // });
+
+       app.get('/', function(req, res) {
+        res.render('test.ejs');
     });
 
     // PROFILE SECTION =========================
